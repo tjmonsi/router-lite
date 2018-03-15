f184bc5bcdaa0621707dc3f7e5da3de997ee38b0
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-03-15 11:00:56 +0800

    added parts
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

---
+++ router-lite.js
@@ -0,0 +1,12 @@
+/// <reference path="typings/global.d.ts"/>
+
+import { ElementLiteBase } from '../element-lite/element-lite-base.js';
+
+/**
+ * ElementLite is a set of methods
+ * @extends {ElementLiteBase}
+*/
+
+class RouterLite extends ElementLiteBase(window.HTMLElement) {
+
+}
