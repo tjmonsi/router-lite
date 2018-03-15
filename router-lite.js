bb1e71d6768101da31a1fcebd78c2163d4c29265
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-03-15 13:13:17 +0000

    add finish

--- router-lite.js
+++ router-lite.js
@@ -1,12 +1,10 @@
 /// <reference path="typings/global.d.ts"/>
 
-import { ElementLiteBase } from '../element-lite/element-lite-base.js';
-
 /**
  * ElementLite is a set of methods
  * @extends {ElementLiteBase}
 */
 
-class RouterLite extends ElementLiteBase(window.HTMLElement) {
+class RouterLite extends window.HTMLElement {
 
 }
