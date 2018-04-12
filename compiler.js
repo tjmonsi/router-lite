f677ceb198494482556f63b53428b1d10b9f4ce0
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2018-04-12 13:59:05 +0000

    Fixed compiler.js

--- compiler.js
+++ compiler.js
@@ -1,23 +1,32 @@
 const fs = require('fs');
+const file = './node_modules/path-to-regexp/index.js';
 
-let string = fs.readFileSync('./node_modules/path-to-regexp/index.js', 'utf8');
-const strings = [
-  'module.exports = pathToRegexp',
-  'module.exports.parse = parse',
-  'module.exports.compile = compile',
-  'module.exports.tokensToFunction = tokensToFunction',
-  'module.exports.tokensToRegExp = tokensToRegExp'
-];
-
-const additions = [
-  '\nexport default pathToRegexp;',
-  '\nexport { parse, compile, tokensToFunction, tokensToRegExp };'
-];
-
-for (let str of strings) {
-  string = string.replace(str, '');
+try {
+  if (!fs.existsSync(file)) return console.log('Continuing installation...');
+  
+  let string = fs.readFileSync(file, 'utf8');
+  const strings = [
+    'module.exports = pathToRegexp',
+    'module.exports.parse = parse',
+    'module.exports.compile = compile',
+    'module.exports.tokensToFunction = tokensToFunction',
+    'module.exports.tokensToRegExp = tokensToRegExp'
+  ];
+  
+  const additions = [
+    '\nexport default pathToRegexp;',
+    '\nexport { parse, compile, tokensToFunction, tokensToRegExp };'
+  ];
+  
+  for (let str of strings) {
+    string = string.replace(str, '');
+  }
+  
+  string += additions.join(' ');
+  
+  fs.writeFileSync('./lib/path-to-regexp.js', string, 'utf8');  
+} catch (error) {
+  console.log(error);
 }
 
-string += additions.join(' ');
 
-fs.writeFileSync('./lib/path-to-regexp.js', string, 'utf8');
